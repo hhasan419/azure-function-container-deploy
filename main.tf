e267e7aa739619f2823af7c7f55560310015311d
hhasan419 <hamidhasan12@gmail.com>
2025-07-19 14:29:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,45 @@
-providers "azurerm" {
-  feature {}
+# create Azure function app with a resource group and app service plan
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+resource "azurerm_app_service_plan" "service_plan" {
+  name                = var.app_service_plan_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku {
+    tier     = "Standard"
+    size     = "S1"
+  }
 }
 
-resources "azurerm_resource_group" "rg"  {
-  name  = "rg-hackathon-demo"
-  location = "East US"
+# create storage account for the function app
+resource "azurerm_storage_account" "dev" {  
+    name                     = var.app_storage_name
+    resource_group_name      = azurerm_resource_group.dev.name
+    location                 = azurerm_resource_group.dev.location
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+    }
+
+resource "azurerm_function_app" "dev" {
+  name                       = var.app_service_name
+  location                   = azurerm_resource_group.dev.location
+  resource_group_name        = azurerm_resource_group.dev.name
+  app_service_plan_id        = azurerm_app_service_plan.dev.id
+  storage_account_name       = azurerm_storage_account.dev.name
+  storage_account_access_key = azurerm_storage_account.dev.primary_access_key
+
+  app_settings = {
+    FUNCTIONS_WORKER_RUNTIME = "node"
+    WEBSITE_NODE_DEFAULT_VERSION = "~22"
+  }
 }
 
-resource "azurerm_storage_account" "storage" {
-  name = "hackathonstroage123"
-  resource_group_name = azure_resource_group.rg.name
-  location =  azure_resource_group.rg.location
-  account_tier = "standard"
-  account_replication_type = "LRS"
+output "app_service_plan_id" {
+  value = azurerm_app_service_plan.dev.id
 }
 
-resouces "azurerm_app_service_plan" "plan" {
-  name = func-app_plan
-  location = azurerm_resource_group.rg.location
-  resource_group_name = azure_resource_group.rg.name
-
-sku {
-  tier = "Dynamic"
-  size = "Y1"
+output "function_app_name" {
+  value = azurerm_function_app.dev.name
 }
-
- os_type = "Windows"
-}
-  
