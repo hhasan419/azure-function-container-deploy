093af950734d1b618a5250fd06f1fba257dd3a1a
hhasan419 <hamidhasan12@gmail.com>
2025-07-19 14:24:23 +0530

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 3.90.0"
+    }
+  }
+  required_version = ">= 1.6.6"
+}
+
+provider "azurerm" {
+   feature {}
+
+    client_id = lqg8Q~7I~HW42Z63J4wf8jT.UTM~iJC-32QJPdkZ
+    client_folder = 0b889ca3-c111-47fc-b0ed-88a37593b8fa
+    subscription_id =  90de0c98-2d25-4094-a834-32b29fdf8003
+    tenant_id = 0b889ca3-c111-47fc-b0ed-88a37593b8fa
+   
