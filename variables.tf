eef5e050feab46d82b2756fd55ed7d18876ff290
hhasan419 <hamidhasan12@gmail.com>
2025-07-19 14:30:39 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,45 @@
+variable "subscription_id" {
+  description = "The Azure subscription ID"
+  default     = "your-subscription-id"
+}
+variable "tenant_id" {
+  description = "The Azure tenant ID"
+  default     = "your-tenant-id"
+}
+
+variable "env" {
+  description = "The environment for the deployment"
+  default     = "dev"
+}
+variable "region" {
+  description = "The AWS region to deploy the infrastructure"
+  default     = "us-east-1"
+}
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  default     = "${env}_ResourceGroup"
+}
+variable "location" {
+  description = "The location of the resource group"
+  default     = "East US"  # eastus1
+}
+variable "app_service_name" {
+  description = "The name of the app service"
+  default     = "${env}AppService"
+}
+
+variable "app_service_plan_name" {
+  description = "The name of the app service plan"
+  default     = "${env}-app-service-plan"
+  
+}
+
+variable "function_app_name" {
+  description = "The name of the function app"
+  default     = "${env}-appointment-service-FunctionApp"
+}
+
+variable "app_storage_name" {
+  description = "The name of the storage account"
+  default     = "${env}StorageAccount"
+}
