21e9fb9751f35c49c2512f277f2c6a0232d87532
hhasan419 <hamidhasan12@gmail.com>
2025-07-19 15:18:42 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -11,8 +11,5 @@ terraform {
 provider "azurerm" {
    feature {}
 
-    client_id = lqg8Q~7I~HW42Z63J4wf8jT.UTM~iJC-32QJPdkZ
-    client_folder = 0b889ca3-c111-47fc-b0ed-88a37593b8fa
-    subscription_id =  90de0c98-2d25-4094-a834-32b29fdf8003
-    tenant_id = 0b889ca3-c111-47fc-b0ed-88a37593b8fa
+}
    
